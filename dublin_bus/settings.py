efc0468013e131d20f8ee0843db93ad1b82f455b
Jake <jake.kavanagh@ucdconnect.ie>
2017-06-19 15:06:53 +0100

    still playing with the server

--- dublin_bus/settings.py
+++ dublin_bus/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dublin_bus_db',
         'USER': 'postgres',
-        'PASSWORD': 'JamesPK94',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
