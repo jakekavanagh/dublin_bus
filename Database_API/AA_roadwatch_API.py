78e2eb231a21d85589fba18890881c7b9278b21d
lyycindy <cindy.lam@ucdconnect.ie>
2017-06-21 11:44:24 +0100

    Updating the AA_roadwatch file, so far have a twitter stream, still work in progress

--- Database_API/AA_roadwatch_API.py
+++ Database_API/AA_roadwatch_API.py
@@ -1,8 +1,23 @@
-import requests
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
 
-url = "http://schemas.datacontract.org/2004/07/WCF.AAIreland.Data"
+consumer_key = "IN2I9yFJ1tehBl9321VJYWO8d"
+consumer_secret = "QR0T5hpBPn3CvaXIwzE7S9fIwAsfVlPtWOhth22VqBfaN0e9sH"
+access_token = "876794679702032385-qBLuTCnaqiXBWcqLkohhPgbfNGQF9bx"
+access_secret = "jUkcx29kXg4R3r7VLVuBhtmTwj0mEvK59k594rtACAoop"
 
-response = requests.get(url)
-result = response.json()
 
-print(result)
+class listener(StreamListener):
+    def on_data(self, data):
+        print(data)
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["Dublin"])
+
