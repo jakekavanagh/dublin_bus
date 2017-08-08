21fc1c6fd90aaa495704750e429c524e4dbac0fd
Jake <jake.kavanagh@ucdconnect.ie>
2017-08-08 11:53:32 +0100

    fixing settings

--- dublin_bus/settings.py
+++ dublin_bus/settings.py
@@ -128,5 +128,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'index/')
 
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder"
+)
+
 
 
