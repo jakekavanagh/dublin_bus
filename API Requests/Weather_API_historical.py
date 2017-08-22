750e215113b6029f12a6ce1d110de4dac14f7e55
lyycindy <cindy.lam@ucdconnect.ie>
2017-08-22 12:42:25 +0100

    Code clean up for file

--- API Requests/Weather_API_historical.py
+++ API Requests/Weather_API_historical.py
@@ -4,7 +4,8 @@ import math
 
 
 def weather_scraper(year, month, day):
-    # Take date as input
+    """Function contact wunderground API and returns weather information according to 3 inputs year month and day.
+    Then writes each line to a csv file"""
     date = year + month + day
 
     urlstart = 'http://api.wunderground.com/api/37d281e3f1931e1e/history_'
@@ -13,31 +14,25 @@ def weather_scraper(year, month, day):
     data = requests.get(url).json()
 
     for i in data['history']['observations']:
-        # weather_array = []
         if 'METAR' in i['metar']:
-            # if i['date']['min'] == "00":
             datetime = year + "-" + month + "-" + day + " " + \
                        i['date']['hour'] + ':' + i['date']['min'] + ':00'
-            # print("Time:", datetime)
             summary = i['conds']
-            # print(summary)
             temp = str(math.floor(float(i["tempm"])))
-            # print("Temp: ", temp)
             rain = i['rain']
-            # print("rain: ", rain)
             wind_speed = str(math.floor(float(i['wspdm'])))
-            # print("wind speed: ", wind_speed)
+            # Store relevant informaion in a specific format in an array
             weather_array = [datetime, summary, temp, rain, wind_speed]
-            # print(weather_array)
-
+            #Write each line to a csv file
             write_to.writerow(weather_array)
 
-
+# Create a new file to store the information
 weather_csv = open("weather_csv_halfhourly.csv", 'w', newline='')
 write_to = csv.writer(weather_csv, dialect='excel')
 weather_info = ["DateTime", "Summary", "Temp", "Rain", "Wind_Speed"]
 write_to.writerow(weather_info)
 
+# Days needed for the data
 nov_days = ["06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23",
             "24", "25", "26", "27", "28", "29", "30"]
 jan_days = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18",
